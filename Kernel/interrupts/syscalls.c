df2a1a0c51f332cbde62b85ab55905044ac17c47
lpercich <lpercich@itba.edu.ar>
2024-10-22 17:06:38 -0300

    syscallmanager

--- Kernel/interrupts/syscalls.c
+++ Kernel/interrupts/syscalls.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
-//uint64_t sysCallManager()
+
 typedef struct {
     uint64_t r15, r14, r13, r12, r11, r10, r9, r8, rsi, rdi, rbp, rdx, rcx, rbx, rax;
 } Reg;
@@ -16,5 +16,13 @@ size_t sys_read(unsigned int fd, char *buffer, size_t count) {
 //OJO FALTA IMPLEMENTAR EL KEYBOARD MANAGER (driver)
 }
 size_t sys_write(unsigned int fd, const char *buffer, size_t count) {
+
+}
+
+uint64_t sysCallManager(Reg * registers) {
+    switch(registers->rax) {
+        case 3: sys_read(registers->rbx, registers->rcx, registers->rdx);
+        case 4: sys_write(registers->rbx, registers->rcx, registers->rdx);
+    }
 
 }
