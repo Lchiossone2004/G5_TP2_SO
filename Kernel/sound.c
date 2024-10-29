e5a16e6ca8f2cf020274e132dc6c30956012bf61
lpercich <lpercich@itba.edu.ar>
2024-10-29 18:20:20 -0300

    ruido cuando perdes

--- Kernel/sound.c
+++ Kernel/sound.c
@@ -20,7 +20,7 @@
  	}
  }
  
- //make it shut up
+ 
  static void nosound() {
  	uint8_t tmp = inb(0x61) & 0xFC;
      
@@ -33,12 +33,23 @@
 
     }
  }
- //Make a beep
- void beep() {
+
+ void beep(int flag) { //si el flag esta en 1 es q perdio
+    if(flag) {
+    play_sound(100);
+    timer_wait(3);
+    play_sound(700);
+    timer_wait(3);
+    play_sound(300);
+    timer_wait(3);
+    }
+    else {
     play_sound(500);
     timer_wait(2);
     play_sound(1000);
     timer_wait(2);
+    }
  	nosound();
-          //set_PIT_2(old_frequency);
+        
  }
+ 
