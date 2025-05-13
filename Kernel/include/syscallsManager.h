d1370cf12ca1de04832ff7718657f24c1b0128b6
Lchiossone2004 <lchiossone@itba.edu.ar>
2025-05-13 16:15:06 -0300

    syscall rework and addition of push/popNoRax

--- Kernel/include/syscallsManager.h
+++ Kernel/include/syscallsManager.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "../memory/memory_manager.h"
+#include "../include/keyboard.h"
 
 typedef struct {
     uint64_t rdi, rsi, rdx, rcx, r8, r9;
@@ -13,24 +14,24 @@ typedef struct {
 uint64_t syscallsManager(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
 
 // Todas las funciones con firma unificada
-void sys_registers_print(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_getChar(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_read(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_write(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_newLine(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_zoomIn(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_zoomOut(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_sleep(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_clear(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_putPixel(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_getTime(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_getKey(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_ranN(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_clearBuffer(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_delete_video(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_test_mm(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_malloc(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_free(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
-void sys_get_memory_info(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_registers_print(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_getChar(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_read(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_write(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_newLine(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_zoomIn(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_zoomOut(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_sleep(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_clear(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_putPixel(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_getTime(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_getKey(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_ranN(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_clearBuffer(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_delete_video(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_test_mm(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_malloc(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_free(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+uint64_t sys_get_memory_info(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
 
 #endif // SYSCALLS_H
--- Kernel/interrupts/syscallsManager.c
+++ Kernel/interrupts/syscallsManager.c
@@ -25,7 +25,7 @@ static size_t total_allocated = 0;
 static size_t total_freed = 0;
 static size_t current_blocks = 0;
 
-typedef void (*syscall_fn)(uint64_t, uint64_t, uint64_t, uint64_t);
+typedef uint64_t (*syscall_fn)(uint64_t, uint64_t, uint64_t, uint64_t);
 
 extern void _sti();
 
@@ -64,14 +64,15 @@ uint64_t syscallsManager(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx,
     return 0;
 }
 
-void sys_registers_print(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_registers_print(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int fd = (unsigned int) rsi;
     if(fd == STDOUT){
         printRegisters();
     }
+    return 0;
 }
 
-void sys_getChar(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_getChar(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int fd = (unsigned int) rsi;
     char *letter = (char *) rdx;
     size_t count = (size_t) rcx;
@@ -90,27 +91,29 @@ void sys_getChar(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
             imprimirVideo(letter, 1, BLANCO);
         }
     }
+    return 0;
 }
 
-void sys_read(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+uint64_t sys_read(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     unsigned int fd = (unsigned int) rsi;
     char *buffer = (char *) rdx;
     size_t count = (size_t) rcx;
 
     if(fd == STDIN){
         if(isBufferEmpty()) {
-            return;
+            return 0;
         }
         int current = getCurr();
         for(int i = 0; i < count && i <= current ; i++) {
             buffer[i] = getFromBuffer(i);
         }
     }
+    return 0;
 }
 
-void sys_write(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+uint64_t sys_write(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     unsigned int fd = (unsigned int) rsi;
-    const char *buffer = (const char *) rdx;
+    char *buffer = (char *) rdx;
     size_t count = (size_t) rcx;
 
     if(fd == STDOUT){
@@ -119,32 +122,36 @@ void sys_write(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     if(fd == STDERR){
         imprimirVideo(buffer, count, ROJO);
     }
+    return 0;
 }
 
-void sys_zoomIn(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){ 
+uint64_t sys_zoomIn(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){ 
     unsigned int fd = (unsigned int) rsi;
     if(fd == STDOUT) {
         zoomIN();
         rePrint();
     }
+    return 0;
 }
 
-void sys_zoomOut(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_zoomOut(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int fd = (unsigned int) rsi;
     if(fd == STDOUT) {
         zoomOUT();
         rePrint();
     }
+    return 0;
 }
 
-void sys_newLine(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_newLine(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int fd = (unsigned int) rsi;
     if(fd == STDOUT){
         nlVideo();
     }
+    return 0;
 }
 
-void sys_sleep(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_sleep(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     int ticks = (int) rsi;
 
     _sti();
@@ -152,16 +159,18 @@ void sys_sleep(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     while(max > ticks_elapsed()){
         _hlt();
     }
+    return 0;
 }
 
-void sys_clear(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_clear(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int fd = (unsigned int) rsi;
     if(fd == STDOUT){
         videoClear();
     }
+    return 0;
 }
 
-void sys_putPixel(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+uint64_t sys_putPixel(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     int posx = (int) rsi;
     int posy = (int) rdx;
     uint32_t color = (uint32_t) rcx;
@@ -170,9 +179,10 @@ void sys_putPixel(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     if(fd == STDOUT) {
         putPixel(color, posx, posy);
     }
+    return 0;
 }
 
-void sys_getTime(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+uint64_t sys_getTime(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     unsigned int fd = (unsigned int) rsi;
     char *ret = (char *) rdx;
 
@@ -181,9 +191,10 @@ void sys_getTime(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
         ret[1] = getMins();
         ret[2] = getSec();
     }
+    return 0;
 }
 
-void sys_getKey(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+uint64_t sys_getKey(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     unsigned int fd = (unsigned int) rsi;
     char *buffer = (char *) rdx;
 
@@ -191,68 +202,77 @@ void sys_getKey(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
     if(fd == STDIN && !isBufferEmpty()){
         *buffer = getBuffer();
     }
+    return 0;
 }
 
-void seed_changer() {
+uint64_t seed_changer() {
     seed = (getHours() * 3600 + getMins() * 60 + getSec());
+    return 0;
 }
 
-void sys_ranN(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_ranN(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     int *toRan = (int *) rsi;
     seed_changer();
     *toRan = (PARA_ALEATORIOS_1 * seed + PARA_ALEATORIOS_2);
     if(*toRan < 0){
         *toRan = -*toRan;
     }
+    return 0;
 }
 
-void sys_clearBuffer(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_clearBuffer(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     while(!isBufferEmpty()){
         getBuffer();
     }
+    return 0;
 }
 
-void sys_delete_video(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_delete_video(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     unsigned int cant = (unsigned int) rsi;
     for(int i = 0; i < cant - 1; i++){
         deleteVideo();
     }
+    return 0;
 }
 
-void sys_test_mm(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_test_mm(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     // aún no implementada
+    return 0;
 }
 
-void sys_malloc(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_malloc(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     void **ptr = (void **) rsi;
     size_t size = (size_t) rdx;
 
-    if (size == 0) return;
+    if (size == 0) return 0;
 
     *ptr = mm_malloc(size);
     if (*ptr != NULL) {
         total_allocated += size;
         current_blocks++;
     }
+    return 0;
 }
 
-void sys_free(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_free(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     void *ptr = (void *) rsi;
-    if (ptr == NULL) return;
+    if (ptr == NULL) return 0;
 
     size_t freed_size = mm_free(ptr);
     total_allocated -= freed_size;
     total_freed += freed_size;
     current_blocks--;
+    return 0;
 }
 
-void sys_get_memory_info(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
+uint64_t sys_get_memory_info(uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8){
     memory_info_t *info = (memory_info_t *) rsi;
-    if (info == NULL) return;
+    if (info == NULL) return 0;
 
     mm_get_info(info);
     info->total_allocated = total_allocated;
     info->total_freed = total_freed;
     info->current_blocks = current_blocks;
     info->memory_leak = (total_allocated > total_freed);
+    return 0;
 }
