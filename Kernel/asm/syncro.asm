92addbc7d0b834c9b37adca8c22736fe97a6eee8
augustolanari <141185506+augustolanari@users.noreply.github.com>
2025-05-30 16:55:55 -0300

    fixes in syncro

--- Kernel/asm/syncro.asm
+++ Kernel/asm/syncro.asm
@@ -1,26 +1,14 @@
-GLOBAL _xadd
-GLOBAL _xchg
 GLOBAL lock_acquire
 GLOBAL lock_release
 
-_xadd:
-  mov rax, rdi
-  lock xadd [rsi], eax
-  ret
-
-_xchg:
-  mov rax, rsi
-  xchg [rdi], eax
-  ret
-
 lock_acquire:
-  mov al, 0
-.retry:
-  xchg [rdi], al
-  test al, al
-  jz .retry
-  ret
+    mov al, 1
+.try:
+    xchg [rdi], al
+    cmp al, 1
+    jne .try
+    ret
 
 lock_release:
-  mov byte [rdi], 1
-  ret
+    mov byte [rdi], 1
+    ret
