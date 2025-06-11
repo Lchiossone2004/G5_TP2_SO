46f4a5ebb5a3afa763203a56d559449761058b96
augustolanari <141185506+augustolanari@users.noreply.github.com>
2025-06-11 15:49:08 -0300

    Revert "quick fix en gmm"
    This reverts commit 2d47a064f0360460f8fa2bb0fdbd881b6d53fa64.

--- Kernel/memory/group_memory_manager.c
+++ Kernel/memory/group_memory_manager.c
@@ -84,16 +84,14 @@ static void *group_init(void *start, size_t size) {
 
 static void *group_malloc(size_t size) {
     if (size == 0 || !free_list || current_blocks >= MAX_BLOCKS) return NULL;
-    size_t req = ALIGN_UP(size < MIN_BLOCK_SIZE ? MIN_BLOCK_SIZE : size);
-
+    size_t req = ALIGN_UP(size);
     block_header_t *cur = free_list;
     do {
         if (cur->size >= req) {
-                size_t rem = cur->size - req;
-                size_t aligned_rem = (rem > HEADER_SIZE)? ALIGN_UP(rem - HEADER_SIZE): 0;
-                if (aligned_rem >= MIN_BLOCK_SIZE) {
-                   block_header_t *nb = (void *)((char *)cur + HEADER_SIZE + req);
-                   nb->size      = aligned_rem;
+            size_t rem = cur->size - req;
+            if (rem > HEADER_SIZE + MIN_BLOCK_SIZE) {
+                block_header_t *nb = (void *)((char *)cur + HEADER_SIZE + req);
+                nb->size      = ALIGN_UP(rem - HEADER_SIZE);
                 nb->user_size = 0;
                 nb->is_free   = true;
                 nb->next = cur->next;
